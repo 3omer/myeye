fc4f65aca1991b9e5cbf53e66a5f948d98c1bb44
Omar M. Osman <omarosmandev@gmail.com>
2021-02-03 01:15:06 +0200

    create upload/download directories on startup

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 const path = require('path')
+const fs = require("fs")
 const express = require("express")
 const morgan = require('morgan')
 const multer = require("multer")
@@ -9,6 +10,17 @@ const reportsRouter = require("./routes/reports")
 // middlewares
 app.use(express.json())
 app.use(morgan("dev"))
+
+const UPLOAD_DIR = path.join(__dirname, process.env.UPLOAD_DIR)
+const DOWNLOAD_DIR = path.join(__dirname, process.env.DOWNLOAD_DIR)
+
+if (!fs.existsSync(UPLOAD_DIR)) {
+    fs.mkdirSync(UPLOAD_DIR)
+}
+
+if (!fs.existsSync(DOWNLOAD_DIR)) {
+    fs.mkdirSync(DOWNLOAD_DIR)
+}
 
 // setting static directory
 app.use('/download', express.static(path.join(__dirname, process.env.DOWNLOAD_DIR)))
@@ -27,7 +39,7 @@ app.use((error, req, res, next) => {
     }
 })
 
-app.listen( process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log('SERVER RUNING : ' + process.env.PORT)
-    
+
 })
